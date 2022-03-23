5dafb6b71105387b61c24f2996f6f1fda65ea7d4
Kayla <kaylatam2@gmail.com>
2022-03-23 13:21:22 -0700

    6 reject

--- problems/06-reject.js
+++ problems/06-reject.js
@@ -21,8 +21,15 @@ let hasA = function(s) {
 console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
 *******************************************************************************/
 
-let reject = function() {
-
+let reject = function (arr, cb) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let ele = arr[i];
+        if (!(cb(ele))) {
+            newArr.push(ele);
+        }
+    }
+    return newArr;
 };
 
 
